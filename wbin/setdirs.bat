6130ebdf0a0abe920a60fa535fb1c92ce4dc8a27
Dirk Pranke <dpranke@chromium.org>
2013-05-09 13:18:16 -0700

    update setdirs

--- wbin/setdirs.bat
+++ wbin/setdirs.bat
@@ -1,2 +1,5 @@
-@set PATH=%PATH%;d:\src\depot_tools;d:\src\depot_tools\python_bin;d:\src\depot_tools\svn_bin
-@"c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\bin\vcvars32.bat"
+@set PATH=%PATH%;e:\src\depot_tools;
+@set PATH=%PATH%;e:\src\depot_tools\python_bin
+@set PATH=%PATH%;e:\src\depot_tools\svn_bin
+@set PATH=%PATH%;e:\src\depot_tools\git_bin
+@"c:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
