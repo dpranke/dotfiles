b91cea7ffa5ae599c506754da5ed0704d6ed2125
Dirk Pranke <dpranke@gmail.com>
2024-10-13 12:43:28 -0700

    Fix `sv` to handle src|no src

--- bat/sv.bat
+++ bat/sv.bat
@@ -1,3 +1,15 @@
+@echo off
 @set VNAME=%1
-@set CSRC=%SRC%\%VNAME%\src
-@cd/d %CSRC%
+if not exist %SRC%\%1\src goto nosrc
+set CSRC=%SRC%\%1\src
+cd/d %CSRC%
+exit /b
+
+:nosrc
+if not exist %SRC%\%1 goto missing
+set CSRC=%SRC%\%1
+cd/d %CSRC%
+exit /b
+
+:missing
+echo No such view '%1' under %SRC%
